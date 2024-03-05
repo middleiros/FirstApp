976257efa12b1dcc153ffacdf8de5daeab3a30f0
Medeiros <gustavo.silva125@portalsesisp.org.br>
2024-03-05 11:29:19 -0300

    Criar um layout para calcular a idade.
    Criar um layout para validar um emial e o telefone.

--- app/src/main/java/com/gustavomedeiros/firstapp/MainActivity.kt
+++ app/src/main/java/com/gustavomedeiros/firstapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.gustavomedeiros.firstapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.gustavomedeiros.firstapp.databinding.ActivityMainBinding
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+
+    private val binding: ActivityMainBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/gustavomedeiros/firstapp/view/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.gustavomedeiros.firstapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.gustavomedeiros.firstapp.R
+import com.gustavomedeiros.firstapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCalculo.setOnClickListener{
+            findNavController().navigate(R.id.calcFragment)
+        }
+        binding.btnVerifica.setOnClickListener{
+            findNavController().navigate(R.id.verificaFragment3)
+        }
+    }
+}
